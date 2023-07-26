9e341327f296905a465fb33af2fbe892ada5f3a1
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2023-07-26 09:31:14 +0530

    Done

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -8,15 +8,20 @@ import { AppService } from 'src/app/services/app.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
- 
 
-  allUserClick:boolean=false;
-  allApprovedUserClick:boolean=false;
+
+  allUserClick: boolean = false;
+  allApprovedUserClick: boolean = false;
+  allInactiveUserClick: boolean = false;
+  allPaidUsrClick:boolean=false
+
 
   user_data_message: any = 'All Members';
   userInfoDATA: any[] = [];
   allUserCount: any = 0;
+  allInactiveUser: any = 0
   allApprovedUser: any = 0
+  allPaidUser: any = 0
   image: any = ''
   constructor(
     private apiparameter: ApiParameterScript,
@@ -26,8 +31,8 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
     this.image = this.appsevices.getFilePath() + 'storage/'
 
-    this.getUserCount(["Approved", 'All', 'Paid', 'Unpaid'])
-    this.laodMemberInfo('All');
+    this.getUserCount(["Approved", 'All', 'Paid', 'Unpaid', 'Inactive'])
+    this.laodMemberInfo('Inactive');
   }
   getUserCount(countUserList: any) {
     var query = "SELECT COUNT(user_id) as count FROM user_info"
@@ -48,9 +53,35 @@ export class DashboardComponent implements OnInit {
           })
 
           break;
+        case 'Paid':
+          query = "SELECT COUNT(user_id) as count FROM user_info WHERE user_membership_plan_active=4"
+          this.apiparameter.fetchDataFormQuery({ "query": query }).subscribe((res: any) => {
+            console.log(res);
+            if (res.success && res['data'].length > 0) {
 
-        case "All":
+              this.allPaidUser = res['data'][0].count ? res['data'][0].count : 0
+            } else {
+              this.allPaidUser = 0
+            }
+
+          })
+          break;
+
+        case "Inactive":
           query = "SELECT COUNT(user_id) as count FROM user_info WHERE user_membership_plan_active=0"
+          this.apiparameter.fetchDataFormQuery({ "query": query }).subscribe((res: any) => {
+            console.log(res);
+            if (res.success && res['data'].length > 0) {
+
+              this.allInactiveUser = res['data'][0].count ? res['data'][0].count : 0
+            } else {
+              this.allInactiveUser = 0
+            }
+
+          })
+          break;
+        case "All":
+          query = "SELECT COUNT(user_id) as count FROM user_info"
           this.apiparameter.fetchDataFormQuery({ "query": query }).subscribe((res: any) => {
             console.log(res);
             if (res.success && res['data'].length > 0) {
@@ -79,8 +110,10 @@ export class DashboardComponent implements OnInit {
     }
     switch (typeOfUser) {
       case 'Approved':
-        this.allApprovedUserClick=true
-        this.allUserClick=false
+        this.allApprovedUserClick = true
+        this.allUserClick = false
+        this.allInactiveUserClick = false
+        this.allPaidUsrClick=false
         this.user_data_message = "All Approved Members"
         apiData = {
           "projection": ["*"],
@@ -88,15 +121,37 @@ export class DashboardComponent implements OnInit {
         }
         break;
       case 'Paid':
+        this.allUserClick = false
+        this.allApprovedUserClick = false
+        this.allInactiveUserClick = false
+        this.allPaidUsrClick=true
+        this.user_data_message = "All Paid Members"
+        apiData = {
+          "projection": ["*"],
+          "whereConditions": {"user_membership_plan_active": 4 }
+        }
 
         break;
-      case 'All':
-        this.allUserClick=true
-        this.allApprovedUserClick=false
+      case 'Inactive':
+        this.allPaidUsrClick=false
+        this.allUserClick = false
+        this.allApprovedUserClick = false
+        this.allInactiveUserClick = true
         this.user_data_message = "All Recent Register Members"
         apiData = {
           "projection": ["*"],
-          "whereConditions":  { "user_membership_plan_active": 0 }
+          "whereConditions": {"user_membership_plan_active": 0 }
+        }
+        break;
+      case 'All':
+        this.allPaidUsrClick=false
+        this.allUserClick = true
+        this.allApprovedUserClick = false
+        this.allInactiveUserClick = false
+        this.user_data_message = "Total Members"
+        apiData = {
+          "projection": ["*"],
+          "whereConditions": {  }
         }
         break;
       default:
