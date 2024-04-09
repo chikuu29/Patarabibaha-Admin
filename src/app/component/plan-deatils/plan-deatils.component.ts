065fb647fe5072ffdc37a93c1ed251be8dc2a910
Gyana1995 <100307598+Gyana1995@users.noreply.github.com>
2024-04-09 23:03:54 +0530

    gyana
    ok

--- src/app/component/plan-deatils/plan-deatils.component.ts
+++ src/app/component/plan-deatils/plan-deatils.component.ts
@@ -12,7 +12,7 @@ import { AppService } from 'src/app/services/app.service';
 export class PlanDeatilsComponent implements OnInit {
 
   panelOpenState = false;
-  plan: any;
+  plan: any = {};
   planRemaingFeature: any
   Object: any = Object;
   keyMapping: any = {
@@ -35,6 +35,7 @@ export class PlanDeatilsComponent implements OnInit {
     "membership_plan_currency":"Membership Currency",
     "membership_plan_chating": "Membership Chating",
   };
+  editedplandetails: any;
   constructor(
    // private planservice: PlanService,
     private ApiParameterScript: ApiParameterScript,
@@ -46,6 +47,7 @@ export class PlanDeatilsComponent implements OnInit {
     this.activatedroute.params.subscribe((res:any)=>{
         if(res.id != ''){
           this.getAllplan(res.id);
+          this.editedPlan(res.id);
         }
     })
 
@@ -58,11 +60,11 @@ export class PlanDeatilsComponent implements OnInit {
     }
     this.ApiParameterScript.fetchdata('user_plan_deatils', param1).subscribe((getprofile_res: any) => {
 
-
       if (getprofile_res.success && getprofile_res['data'].length > 0) {
         this.plan = getprofile_res['data'];
         console.log(this.plan);
-
+      }else{
+        this.plan = [];
       }
     })
 
@@ -75,12 +77,25 @@ export class PlanDeatilsComponent implements OnInit {
       "whereConditions": { "user_id": data }
     }
     this.ApiParameterScript.fetchdata('edited_plan_details', param).subscribe((getprofile_res: any) => {
-
-
       if (getprofile_res.success && getprofile_res['data'].length > 0) {
         this.planRemaingFeature = getprofile_res['data'][0];
       }
     })
+  }
+
+  editedPlan(data:any){
+    let param1 = {
+      "projection": ['*'],
+      "whereConditions": { "user_id": data }
+    }
+    this.ApiParameterScript.fetchdata('left_plan', param1).subscribe((getprofile_res: any) => {
+
+      if (getprofile_res.success && getprofile_res['data'].length > 0) {
+          this.editedplandetails = getprofile_res['data'];
+          console.log(this.editedplandetails);
+
+      }
+    });
   }
 
 }
