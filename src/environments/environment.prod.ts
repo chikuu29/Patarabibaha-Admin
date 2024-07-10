9a72bccca7a8599de5527786639148f99068c00c
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2024-07-10 22:44:54 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  fontendUrl:"https://choicemarriage.com/",
-  baseApiURL:'https://choicemarriage.com/backend-api/public/',
-  filePath:"https://choicemarriage.com/",
-  application_url:"https://choicemarriage.com/"
+  fontendUrl:"https://patara.patarabibaha.com/",
+  baseApiURL:'https://patara.patarabibaha.com/backend-api/public/',
+  filePath:"https://patara.patarabibaha.com/",
+  application_url:"https://patara.patarabibaha.com/"
 };
